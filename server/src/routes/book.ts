38f9268e7cd465c42a6f703d1c0e3903f13776d4
Roger Ngo <urbanspr1nter@gmail.com>
2020-07-03 11:23:52 -0700

    Book, and login route implementations.

--- server/src/routes/book.ts
+++ server/src/routes/book.ts
@@ -1,11 +1,22 @@
 import { IRouter } from "express";
+import * as db from '../mockDb.json'
 
 export function mountBook(app: IRouter) {
     app.get('/book/search/title/:bookTitle', (req, res) => {
-        res.status(200).send(`bookTitle: ${req.params.bookTitle}`);
+        const bookTitle = req.params.bookTitle.trim().toLowerCase();
+        const books = db.books;
+
+        const results = books.filter(b => b.title.toLowerCase().indexOf(bookTitle) !== -1);
+        
+        res.status(200).json(results);
     });    
 
     app.get('/book/search/author/:authorName', (req, res) => {
-        res.status(200).send(`authorName: ${req.params.authorName}`);
+        const authorName = req.params.authorName.trim().toLowerCase();
+        const books = db.books;
+        
+        const results = books.filter(b => b.author.toLowerCase().indexOf(authorName) !== -1);
+        
+        res.status(200).json(results);
     });
 }
--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,7 +1,46 @@
 import { IRouter } from "express";
+import * as bcrypt from 'bcrypt';
+import * as db from '../mockDb.json';
+
+const classrooms = db.classrooms;
+const students = db.students;
+const users = db.users;
+
+function hashPassword(plainTextPassword: string) {
+    const saltRounds = 7;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashed = bcrypt.hashSync(plainTextPassword, salt);
+
+    return {
+        salt,
+        hashed
+    };
+}
+
+function isValidHash(plainTextPassword: string, hashed: string, salt: string) {
+    const testHashed = bcrypt.hashSync(plainTextPassword, salt);
+
+    return hashed === testHashed;
+}
 
 export function mountLogin(app: IRouter) {
     app.post('/login', (req, res) => {
-        res.status(200).send(req.body);
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if (!username || !password)
+            return res.status(400).json({message: 'Must provide username, and password.'});
+
+        const user = users.find(u => u.username === username);
+
+        if (!user)
+            return res.status(404).json({message: 'Could not find user by the username.'});
+
+        if (!isValidHash(password, user.password, user.salt))
+            return res.status(401).json({message: 'Invalid password.'});
+
+        const classroom = classrooms.find(c => c.id === students.find(s => s.userId === user.id).classroomId);
+
+        return res.status(200).json({message: `Welcome to ${classroom.name}`});
     });
 }
